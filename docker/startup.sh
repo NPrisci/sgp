366c1d2d41a8e028a36874f040d5fa828107f584
NPrisci <priscilialauress@gmail.com>
2025-08-08 18:19:00 +0200

    Update startup.sh

--- docker/startup.sh
+++ docker/startup.sh
@@ -4,8 +4,10 @@ set -e
 echo "✅ Installation des dépendances Laravel"
 composer install --no-interaction --prefer-dist --optimize-autoloader
 
-echo "✅ Création de la table notifications si nécessaire"
-php artisan make:notifications-table
+echo "✅ Nettoyage et mise en cache de la configuration"
+php artisan config:clear
+php artisan config:cache
+
 
 
 echo "✅ Lancement des migrations (fresh + seed)"
